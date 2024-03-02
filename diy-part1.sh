9dbd43d2d2852810299101fe5f469b54b7dcc5e8
xiangtailiang <xiangtailiang@foxmail.com>
2024-03-02 21:32:07 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+#===============================================
+# Description: DIY script
+# File name: diy-script.sh
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#===============================================
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 修改版本为编译日期
+date_version=$(date +"%Y%m%d%H")
+# sed -i "s/0000000000/${date_version}/g" version
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-# echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
-# echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
+echo $date_version > version
